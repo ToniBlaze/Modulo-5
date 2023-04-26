183a0dd9299d31e2a6b47be6dbe57b348b7d0e76
Alessio <alessio.toninello@gmail.com>
2023-04-26 21:18:12 +0200

    non so come esportatre FIlteredBooks

--- D3/first-app/src/App.js
+++ D3/first-app/src/App.js
@@ -4,12 +4,16 @@ import MyNav from './MyNav';
 import MyFooter from './MyFooter';
 import Welcome from './Welcome';
 import LatestRelease from './LatestRelease';
+import BookFilter from './BookFilter';
 
 function App() {
   return (
     <div className="App">
       <MyNav />
       <Welcome />
+      <hr />
+      <BookFilter />
+      <hr />
       <LatestRelease />
       <MyFooter />
     </div>
---
+++ D3/first-app/src/BookFilter.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+import Horror from "./categorie/horror.json";
+import SingleBook from "./SingleBook";
+
+function BookFilter() {
+  const [searchText, setSearchText] = useState("")
+
+  // Funzione per gestire il cambiamento dell'input di ricerca
+  function handleInputChange(event) {
+    setSearchText(event.target.value);
+  }
+
+
+  // Creazione del nuovo array di libri filtrati in base al testo di ricerca
+  const filteredBooks = Horror.filter((book) =>
+    book.title.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  return (
+    <Container>
+      <Row className="d-flex justify-content-center gap-2">
+        <label> <b>Cerca dei libri:</b></label>
+        <input  type="text" className="text-center" placeholder="*** Cerca i libri ***" value={searchText} onChange={handleInputChange} />
+      </Row>
+    </Container>
+  );
+}
+
+export default BookFilter;
+ 
